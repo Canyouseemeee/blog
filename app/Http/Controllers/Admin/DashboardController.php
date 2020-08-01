4366babc6ff752691e029704ca7f467fc6f1601b
Bank <indyauok@gmail.com>
2020-08-01 23:50:08 +0700

    init

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function registered(){
+        $users = User::all();
+        return view('admin.register',compact('users'));
+    }
+
+    public function registeredit(Request $request, $id){
+        $users = User::findOrFail($id);
+        return view('admin.register-edit',compact('users'));
+    }
+
+    public function registerupdate(Request $request, $id){
+        $users = User::find($id);
+        $users->name = $request->input('username');
+        $users->usertype = $request->input('usertype');
+        $users->update();
+
+        return redirect('/role-register')->with('status','Your Data is Updated');
+    }
+
+    public function registerdelete($id){
+        $users = User::findOrFail($id);
+        $users->delete();
+        return redirect('/role-register')->with('status','Your Data is Deleted');
+    }
+}
